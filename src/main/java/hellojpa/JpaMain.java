d8e5476028a4d3eab92a14ebd6ad67e475e7891d
darren9352 <darren9352@gmail.com>
2020-04-29 00:40:32 +0900

    initial commit

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,33 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Member member = new Member();
+            member.setUsername("B");
+            System.out.println("===========================");
+            em.persist(member);
+            System.out.println(member.getId());
+            System.out.println("===========================");
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,40 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 50)
+public class Member {
+
+    @Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String username;
+
+    public Member(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
